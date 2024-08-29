57a7c7b51e03dbd02f3fc7caf1cfda9e7f30a480
IsCoffeeTho <aaron.menadue@gmail.com>
2024-08-29 17:31:05 +0930

    added close functionality

--- src/websocket.ts
+++ src/websocket.ts
@@ -15,14 +15,28 @@ export default class websocket implements toastiebun.websocket {
 			this.#ws = ws;
 	}
 
-	on(event:string, fn:(...args:any[]) => any) { return this.#ev.on(event, fn); }
-	once(event:string, fn:(...args:any[]) => any) { return this.#ev.once(event, fn); }
-	emit(event:string, ...args:any[]) { return this.#ev.emit(event, ...args); }
+	on(event: string, fn: (...args: any[]) => any) { return this.#ev.on(event, fn); }
+	once(event: string, fn: (...args: any[]) => any) { return this.#ev.once(event, fn); }
+	emit(event: string, ...args: any[]) { return this.#ev.emit(event, ...args); }
 
-	send(m:string|Buffer|Uint8Array, compressed=false):boolean {
+	send(m: string | Buffer | Uint8Array, compressed = false): boolean {
 		if (!this.#ws)
 			return false;
 		this.#ws.send(m, compressed);
 		return true;
+	}
+
+	close(): void;
+	close(reason?: string): void;
+	close(code?: number): void;
+	close(code?: number, reason?: string): void;
+	close(code_or_reason?: number | string, reason?: string) {
+		var code: number | undefined = undefined;
+		if (typeof code_or_reason == "number") {
+			code = code_or_reason
+		} else if (typeof code_or_reason == "string") {
+			reason = code_or_reason;
+		}
+		this.#ws?.close(code, reason);
 	}
 }
