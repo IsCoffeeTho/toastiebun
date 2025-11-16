205144faefa056a7d116c81b8dbf06e796285235
Aaron Menadue <aaron.menadue@gmail.com>
2025-11-16 14:09:36 +1030

    Added mini stress tests

--- tests/mockserver.test.ts
+++ tests/mockserver.test.ts
@@ -15,6 +15,26 @@ const mockserver = new toastie.server()
 	.get("/", (req, res) => {
 		res.send("TEST SERVER");
 	})
+	.get("/test-route", (req, res) => {
+		res.send("Success for Test Route");
+	})
+	.get("/another-test-route", (req, res) => {
+		res.send("Success for Test Route again");
+	})
+	.get("/json-test", (req, res) => {
+		res.send({
+			test: "json"
+		});
+	})
+	.get("/test-file", (req, res) => {
+		res.sendFile(`${__dirname}/../mockserver/test.txt`, () => { res.status(404).send("404"); });
+	})
+	.get("/missing-file", (req, res) => {
+		res.sendFile(`${__dirname}/../mockserver/doesnt-exist.test`, () => { res.status(404).send("404"); });
+	})
+	.get("/appliance", (req, res) => {
+		res.status(418).send("Short and Stout");
+	})
 	.get("/async", async (req, res) => {
 		await new Promise((res, rej) => {
 			setTimeout(res, 50);
@@ -35,26 +55,6 @@ const mockserver = new toastie.server()
 			if (data.toString() == "exit")
 				return ws.close();
 		}
-	})
-	.get("/test-route", (req, res) => {
-		res.send("Success for Test Route");
-	})
-	.get("/another-test-route", (req, res) => {
-		res.send("Success for Test Route again");
-	})
-	.get("/json-test", (req, res) => {
-		res.send({
-			test: "json"
-		});
-	})
-	.get("/test-file", (req, res) => {
-		res.sendFile(`${__dirname}/../mockserver/test.txt`, () => { res.status(404).send("404"); });
-	})
-	.get("/missing-file", (req, res) => {
-		res.sendFile(`${__dirname}/../mockserver/doesnt-exist.test`, () => { res.status(404).send("404"); });
-	})
-	.get("/appliance", (req, res) => {
-		res.status(418).send("Short and Stout");
 	})
 	.get("/increment", (req, res) => {
 		res.send(`${++dynamicCounter}`);
